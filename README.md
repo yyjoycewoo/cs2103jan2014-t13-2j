a2991302c29a2c64b7677a790401b26e5241627e
DarylHo <DarylHo@localhost>
2014-02-17 13:18:33 +0800

    Merge

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello World
+So far so good
+woohoo!
---
+++ src/TaskList.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+/**
+ * Class to store a list of all Tasks that the user wants to keep track of
+ * @author Joyce
+ *
+ */
+public class TaskList {
+	private ArrayList<Task> list;
+	
+	public void addToList(Task t) {
+		list.add(t);
+	}
+	
+	public Task getListItem(int i) {
+		return list.get(i);
+	}
+
+	public ArrayList<Task> getList() {
+		return list;
+	}
+
+	public void setList(ArrayList<Task> list) {
+		this.list = list;
+	}
+}
