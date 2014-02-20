3f4bb94b04aa8e37119ec406555451f33ede1dab
DarylHo <DarylHo@localhost>
2014-02-20 13:43:13 +0800

    minor changes to Controller

--- src/Controller.java
+++ src/Controller.java
@@ -15,10 +15,10 @@ public class Controller {
 		
 		// modify the starting time
 		if(words[0] == "from") {
-			if(words[1] != null || words[1] != ""){
+			if(words[1] != null || words[1] != "") {
 				int time = Integer.parseInt(words[1]);
-				int hr = time/100;
-				int min = time%100;
+				int hr = time / 100;
+				int min = time % 100;
 				list.getListItem(index).setStartTime(new Time(hr, min));
 			}
 		}
