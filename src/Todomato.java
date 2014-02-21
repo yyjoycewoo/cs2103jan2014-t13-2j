41c037e1cc984e96f62d2805fc2cab486c53dc39
Joyce Woo <yyjoycewoo@gmail.com>
2014-02-21 08:43:37 +0800

    Created simple main method in Todomato.java to facilitate testing

--- src/Todomato.java
+++ src/Todomato.java
@@ -1,35 +1,25 @@
+import java.util.Scanner;
+
 
 public class Todomato {
 
+	private static final String WELCOME_MSG = "Welcome to Todomato";
+	private static final String PROMPT_USER_INPUT = "Command: ";
+	private static Scanner scan = new Scanner(System.in);
+
+	/**
+	 * Take in and execute user commands until the user wants to exit.
+	 * @param args
+	 */
 	public static void main(String[] args) {
-
-		System.out.println("Welcome to Todomato");
+		System.out.println(WELCOME_MSG);
 		
-		//This represents 10:07
-		Time sampleStartTime = new Time(10, 07);
-		//This represents 22:40. Minutes are optional
-		Time sampleEndTime = new Time(22, 4);
-		//This represents Feb 8. Year is optional, will only display if it is not the current year.
-		Date sampleDate = new Date(2, 8);
-		String sampleLocation = "library";
-		
-		/* I only implemented the basic info for now: desc (mandatory), start/end time, date, location.
-		 * I made constructors for all possible combinations of the above,
-		 * except you can't have an end time without a start time
-		 */
-		Task sampleTaskDesc = new Task("Meeting");
-		Task sampleTaskDescStart = new Task("Meeting", sampleStartTime);
-		Task sampleTaskDescStartEnd = new Task("Meeting", sampleStartTime, sampleEndTime);
-		Task sampleTaskDescStartLocation = new Task("Meeting", sampleStartTime, sampleEndTime, sampleLocation);
-		Task sampleTaskDescStartEndDate = new Task("Meeting", sampleStartTime, sampleEndTime, sampleDate);
-		Task sampleTaskDescStartEndDateLocation = new Task("Meeting", sampleStartTime, sampleEndTime, sampleDate, sampleLocation);
-		
-		System.out.println(sampleTaskDesc);
-		System.out.println(sampleTaskDescStart);
-		System.out.println(sampleTaskDescStartEnd);
-		System.out.println(sampleTaskDescStartLocation);
-		System.out.println(sampleTaskDescStartEndDate);
-		System.out.println(sampleTaskDescStartEndDateLocation);
-
+		//type "exit" to exit the program
+		while (true) {
+			System.out.print(PROMPT_USER_INPUT);
+			String command = scan.nextLine();
+			String status = CmdHandler.processCommand(command);
+			System.out.println(status);	
+		}
 	}
 }
