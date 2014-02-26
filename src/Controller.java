f07e1592d8ec39315e34e0bf51522804f6743a33
DarylHo <DarylHo@localhost>
2014-02-26 10:11:23 +0800

    added processAdd and other methods that sieves out description, time and date from user input

--- src/Controller.java
+++ src/Controller.java
@@ -5,6 +5,9 @@
  */
 public class Controller {
 
+	private static final int noOfCharInAt = 4;
+	private static final int posOfMinute = 2;
+	
 	private TaskList list;
 	
 	// take in task description and index of the task
@@ -46,8 +49,71 @@ public class Controller {
 		return null;
 	}
 
-	public static Task processAdd(String argument) {
+	public static Task processAdd(String input) {
 		// TODO Auto-generated method stub
-		return null;
+		String taskDes = getTaskDes(input);
+		String timeAndDate = getTimeAndDate(input);
+		Date userDate = getDate(timeAndDate);
+		Time userTime = getTime(timeAndDate);
+		Task userTask = new Task(taskDes, userTime, userDate);
+		return userTask;
 	}
+	
+	/**
+	 * Retrieves the task description from user input
+	 * @param input The raw user input after the command word
+	 * @return string with the task description
+	 */
+	
+	public static String getTaskDes (String input) {
+		String TaskDes = "";
+		int index = checkForAtPosition(input);
+		TaskDes = input.substring(0, index);
+		return TaskDes;
+	}
+	
+	public static int checkForAtPosition(String input) {
+		String checkForAt = " at ";
+		if (input.indexOf(checkForAt) == -1)
+		{
+			return 0;
+		}
+		else
+		{
+			return input.indexOf(checkForAt);
+		}
+	}
+	
+	public static int checkForSpcPos (String input) {
+		String checkForSpc = " ";
+		return input.indexOf(checkForSpc);
+	}
+	
+	public static String getTimeAndDate (String input) {
+		String checkForAt = " at ";
+		int index = input.indexOf(checkForAt);
+		input = input.substring(index + noOfCharInAt);
+		return input;
+	}
+	
+	public static Time getTime(String input) {
+		String delims = " ";
+		int index = input.indexOf(delims);
+		input = input.substring(0, index);
+		String userHour = input.substring(0,posOfMinute);
+		String userMinute = input.substring(posOfMinute);
+		Time userTime = new Time(Integer.parseInt(userHour), Integer.parseInt(userMinute));
+		return userTime;
+	}
+	
+	public static Date getDate (String input) {
+		String delims = "/";
+		String[] tokens = input.split(delims);
+		Date userDate = new Date(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
+		return userDate;
+	}
+	
 }
+	
+	
+
