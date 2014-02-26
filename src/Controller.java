0c04c65cf524bc945a1919dce0b33a2d4b6dad97
this9is3me <this9is3me@localhost>
2014-02-26 18:05:15 +0800

    wrote update method

--- src/Controller.java
+++ src/Controller.java
@@ -1,53 +1,18 @@
-/** For the commands such as add, delete, update, display so on
+/**
+ * For the commands such as add, delete, update, display so on
  * 
- * @author Hao Eng
- *
+ * 
  */
 public class Controller {
 
+	// " at "
 	private static final int noOfCharInAt = 4;
 	private static final int posOfMinute = 2;
-	
-	private TaskList list;
-	
-	// take in task description and index of the task
-	public Task processUpdate(String des, int index){
-		// description can have "from", "on", "to" and "at"
-		// split according to the above keywords
-		String[] words = des.split("\\s+");
-		
-		// modify the starting time
-		if(words[0] == "from") {
-			if(words[1] != null || words[1] != "") {
-				int time = Integer.parseInt(words[1]);
-				int hr = time / 100;
-				int min = time % 100;
-				list.getListItem(index).setStartTime(new Time(hr, min));
-			}
-		}
-		
-		//TBC...
-		return list.getListItem(index);
-	}
-
-	/*Hao Eng, your function should be like this (takes in a string)
-	 * You would have to parse it to get the des and index, then call
-	 * a helper function (refer to image from whiteboard)
-	 */
-	public static Task processUpdate(String argument) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public static TaskList processDisplay() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public static Task processDelete(String argument) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	// " time "
+	private static final int noOfCharInTime = 6, noOfCharInDesc = 6;
+	private static final String INVALID_UPDATE = "No parameter to edit.";
+	private static boolean timeFlag = false, descFlag = false;
+	private static TaskList list;
 
 	public static Task processAdd(String input) {
 		// TODO Auto-generated method stub
@@ -58,62 +23,158 @@ public class Controller {
 		Task userTask = new Task(taskDes, userTime, userDate);
 		return userTask;
 	}
-	
+
+	/**
+	 * @author Hao Eng
+	 * @param argument
+	 * @return Updated Task
+	 */
+	public static Task processUpdate(String argument) {
+		int index = getTaskIndex(argument);
+		int whichToEdit = findDetailToEdit(argument);
+		if (whichToEdit == -1) {
+			printInvalidEdit();
+		} else {
+			if (timeFlag) {
+				return updateTime(index, whichToEdit, argument);
+			}
+			if (descFlag) {
+				return updateDesc(index, whichToEdit, argument);
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * @param index
+	 * @param editTime
+	 * @param argument
+	 *            that contains new time
+	 * @return updated task
+	 */
+	private static Task updateTime(int index, int editTime, String argument) {
+		int time = Integer.parseInt(argument.substring(editTime
+				+ noOfCharInTime));
+		int hr = time / 100;
+		int min = time % 100;
+		list.getListItem(index).setStartTime(new Time(hr, min));
+		return list.getListItem(index);
+	}
+
+	/**
+	 * @param index
+	 * @param editDesc
+	 * @param argument
+	 *            that contains new description
+	 * @return updated task
+	 */
+	private static Task updateDesc(int index, int editDesc, String argument) {
+		list.getListItem(index).setDescription(
+				argument.substring(editDesc + noOfCharInDesc));
+		return list.getListItem(index);
+	}
+
+	private static Task printInvalidEdit() {
+		System.out.println(INVALID_UPDATE);
+		return null;
+	}
+
+	/**
+	 * @param argument
+	 * @return the starting index of which task detail to change
+	 */
+	private static int findDetailToEdit(String argument) {
+		int editTime = argument.indexOf(" time ");
+		if (editTime > -1) {
+			timeFlag = true;
+			return editTime;
+		} else {
+			int editDesc = argument.indexOf(" desc ");
+			if (editDesc > -1) {
+				descFlag = true;
+				return editDesc;
+			} else {
+				return -1;
+			}
+		}
+	}
+
+	/**
+	 * @param argument
+	 * @return task index
+	 * @throws NumberFormatException
+	 */
+	private static int getTaskIndex(String argument)
+			throws NumberFormatException {
+		// remove the command word: edit
+		String input = argument.replaceFirst("update ", "");
+		int spaceAfterIndex = input.indexOf(" ");
+		return (Integer.parseInt(input.substring(0, spaceAfterIndex)));
+	}
+
+	public static Task processDelete(String argument) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static TaskList processDisplay() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static int checkForAtPosition(String input) {
+		String checkForAt = " at ";
+		if (input.indexOf(checkForAt) == -1) {
+			return 0;
+		} else {
+			return input.indexOf(checkForAt);
+		}
+	}
+
+	public static int checkForSpcPos(String input) {
+		String checkForSpc = " ";
+		return input.indexOf(checkForSpc);
+	}
+
+	public static Date getDate(String input) {
+		String delims = "/";
+		String[] tokens = input.split(delims);
+		Date userDate = new Date(Integer.parseInt(tokens[0]),
+				Integer.parseInt(tokens[1]));
+		return userDate;
+	}
+
 	/**
 	 * Retrieves the task description from user input
-	 * @param input The raw user input after the command word
+	 * 
+	 * @param input
+	 *            the raw user input after the command word
 	 * @return string with the task description
 	 */
-	
-	public static String getTaskDes (String input) {
+
+	public static String getTaskDes(String input) {
 		String TaskDes = "";
 		int index = checkForAtPosition(input);
 		TaskDes = input.substring(0, index);
 		return TaskDes;
 	}
-	
-	public static int checkForAtPosition(String input) {
-		String checkForAt = " at ";
-		if (input.indexOf(checkForAt) == -1)
-		{
-			return 0;
-		}
-		else
-		{
-			return input.indexOf(checkForAt);
-		}
-	}
-	
-	public static int checkForSpcPos (String input) {
-		String checkForSpc = " ";
-		return input.indexOf(checkForSpc);
-	}
-	
-	public static String getTimeAndDate (String input) {
+
+	public static String getTimeAndDate(String input) {
 		String checkForAt = " at ";
 		int index = input.indexOf(checkForAt);
 		input = input.substring(index + noOfCharInAt);
 		return input;
 	}
-	
+
 	public static Time getTime(String input) {
 		String delims = " ";
 		int index = input.indexOf(delims);
 		input = input.substring(0, index);
-		String userHour = input.substring(0,posOfMinute);
+		String userHour = input.substring(0, posOfMinute);
 		String userMinute = input.substring(posOfMinute);
-		Time userTime = new Time(Integer.parseInt(userHour), Integer.parseInt(userMinute));
+		Time userTime = new Time(Integer.parseInt(userHour),
+				Integer.parseInt(userMinute));
 		return userTime;
 	}
-	
-	public static Date getDate (String input) {
-		String delims = "/";
-		String[] tokens = input.split(delims);
-		Date userDate = new Date(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
-		return userDate;
-	}
-	
-}
-	
-	
 
+}
