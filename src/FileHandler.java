a98e1b6b745186feb422d9fca1741c17819131be
Foo Lin Xuan <linxuan1994@gmail.com>
2014-02-27 10:11:54 +0800

    added processDisplay and processDelete, method stub for FileHandler.readFile

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -5,6 +5,11 @@
  */
 
 public class FileHandler {
+
+	public static TaskList readFile() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	// I am a file handler
 	// I cannot handle anything yet
 }
