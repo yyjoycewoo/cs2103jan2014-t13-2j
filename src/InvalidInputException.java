fc48db1872d8002bb7ec897e603ba341efc77201
Joyce Woo <yyjoycewoo@gmail.com>
2014-03-09 13:00:33 +0800

    Added Exception class

---
+++ src/InvalidInputException.java
@@ -0,0 +1,16 @@
+
+public class InvalidInputException extends Exception {
+
+	public InvalidInputException(String message) {
+		super(message);
+	}
+	
+    public InvalidInputException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+    
+    public String getMessage()
+    {
+        return super.getMessage();
+    }
+}
