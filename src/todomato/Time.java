842f6fbb07f07b46e89fd8dc44559e015a4d6ce4
Joyce Woo <yyjoycewoo@gmail.com>
2014-03-10 09:04:11 +0800

    Refactored Date and Time to deal with timeString and dateString constructors

--- src/todomato/Time.java
+++ src/todomato/Time.java
@@ -35,23 +35,26 @@ public class Time {
 	 * @throws InvalidInputException 
 	 */
 	public Time(int hour, int min) throws InvalidInputException {
-		if (hour < 0 || hour > 23)
-			throw new InvalidInputException(INVALID_HOUR_SPECIFIED + hour);
-		if (min < 0 || min > 59)
-			throw new InvalidInputException(INVALID_MINUTES_SPECIFIED + min);
+		//throws exception if time is invalid
+		checkIfValidTime(hour, min);
+		
 		this.setHour(hour);
 		this.setMin(min);
-	}
-	
+	}	
 	
 	/**
 	 * Create a new Time object with a standard string
 	 * @param timeString a standard string representing time 00:00
+	 * @throws InvalidInputException 
 	 */
-	public Time(String timeString) {
+	public Time(String timeString) throws InvalidInputException {
 		String[] timeArray = timeString.split(":");
 		int hour = Integer.parseInt(timeArray[0]);
 		int min = Integer.parseInt(timeArray[1]);
+
+		//throws exception if time is invalid
+		checkIfValidTime(hour, min);
+		
 		this.setHour(hour);
 		this.setMin(min);
 	}
@@ -73,6 +76,21 @@ public class Time {
 		}
 		time += String.valueOf(min);
 		return time;
+	}
+	
+	private boolean isValidHour(int hour) {
+		return (hour >= 0 && hour <= 23);
+	}
+	
+	private boolean isValidMin(int min) {
+		return (min >= 0 && min <= 59);
+	}
+	
+	private void checkIfValidTime(int hour, int min) throws InvalidInputException {
+		if (!isValidHour(hour))
+			throw new InvalidInputException(INVALID_HOUR_SPECIFIED + hour);
+		if (!isValidMin(min))
+			throw new InvalidInputException(INVALID_MINUTES_SPECIFIED + min);		
 	}
 	
 	private boolean isOneDigit(int x) {
