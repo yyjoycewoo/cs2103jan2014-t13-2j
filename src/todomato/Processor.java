2d84a73db8963b44fc16431d7ec5639903202f4b
Joyce Woo <yyjoycewoo@gmail.com>
2014-03-12 22:55:03 +0800

    Implemented command execution in GUI, enter text in box and press enter to execute commands.

--- src/todomato/Processor.java
+++ src/todomato/Processor.java
@@ -3,8 +3,8 @@ package todomato;
 import java.util.Stack;
 
 public class Processor {
-	//protected static String fileLoc = "C:\\Users\\Joyce\\Documents\\Year 2\\test.txt";
-	protected static String fileLoc = "D:\\test.txt";
+	protected static String fileLoc = "C:\\Users\\Joyce\\Documents\\Year 2\\test.txt";
+	//protected static String fileLoc = "D:\\test.txt";
 	protected static FileHandler fileHandler = new FileHandler(fileLoc);
 	protected static TaskList list = fileHandler.readFile();
 	protected static Stack<TaskList> oldLists = new Stack<TaskList>();
