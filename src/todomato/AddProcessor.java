7f8927c782cb9d3f9d25efa89602b834c448d8a7
Joyce Woo <yyjoycewoo@gmail.com>
2014-03-19 23:16:01 +0800

    Added assert statement

---
+++ src/todomato/AddProcessor.java
@@ -0,0 +1,300 @@
+package todomato;
+
+import hirondelle.date4j.DateTime;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+/**
+ * This class contains methods to process add commands by the user.
+ * It updates the user's lists of tasks, and saves it to disk.
+ * 
+ * <p>
+ * It can process commands with a description, a start time, an end
+ * time, a date and a location. Only a description is mandatory.
+ * 
+ * <p>
+ * The following keywords are necessary, while the order is flexible:
+ * <ul>
+ * <li>" at " or " from " keyword for the start time
+ * <li>" to ", " until ", " due " for the end time
+ * <li>" on " for the date
+ * <li>" in " for the keyword
+ * </ul>
+ * 
+ * <p>
+ * Example: "add dinner with bob at 1900 on mar 3 in uTown"
+ * 
+ * <p>
+ * The following time formats are supported:
+ * <ul>
+ * <li>930am/pm
+ * <li>9am/pm
+ * <li>1230
+ * <li>0730
+ * <li>0730pm
+ * </ul>
+ * 
+ * <p>
+ * The following date formats are supported (case does not matter):
+ * <ul>
+ * <li>jan 1
+ * <li>1 january
+ * <li>january 1
+ * <li>DD/MM
+ * <li>DD/MM/YY
+ * </ul>
+ * 
+ * @author Daryl
+ * 
+ */
+public class AddProcessor extends Processor {
+
+	private static String[] keywords = new String[] { " at ", " from ",
+			" until ", " to ", " in ", " due ",  " on "};
+	
+	private static int INDEX_OF_WORDS_AFTER_KEYWORDS = 1;
+	private static int NOT_FOUND = -1;
+	//private static String INVALID_DATE = "Invalid date format";
+	//private static String INVALID_TIME = "Invalid time format";
+	private static String INVALID_INPUT = "Invalid input format";
+	private static int INDEX_OF_DESC = 0;
+	private static int INDEX_OF_START_TIME_STRING = 1;
+	private static int INDEX_OF_END_TIME_STRING = 2;
+	private static int INDEX_OF_DATE_STRING = 3;
+	private static int INDEX_OF_LOCATION_STRING = 4;
+	private static int NO_OF_TASK_DETAILS = 5;
+	private static String[] taskDetails = new String[NO_OF_TASK_DETAILS];
+	
+	/**
+	 * Parses Task from a String
+	 * @param input
+	 * @return Task
+	 * @throws InvalidInputException
+	 */
+	public static TaskDT parseTask(String input) throws InvalidInputException {
+		Arrays.fill(taskDetails,null);
+		boolean taskDesExtracted = false;
+		int keywordIndex = -1;
+		TaskDT userTaskDT = null;
+		String[] stringFragments = null;
+		
+		if (!keywordIsInString(input)) {
+			taskDetails[INDEX_OF_DESC] = input;
+		}
+		
+		while (keywordIsInString(input)) {
+			keywordIndex = getFirstKeyword(input);
+			stringFragments = splitByKeyword(input, keywords[keywordIndex]);
+			if (!taskDesExtracted) {
+				taskDetails[INDEX_OF_DESC] = stringFragments[0];
+				taskDesExtracted = true;
+			}
+			taskDetails = keywordHandler(keywordIndex, stringFragments[INDEX_OF_WORDS_AFTER_KEYWORDS]);
+			input = stringFragments[INDEX_OF_WORDS_AFTER_KEYWORDS];
+		}
+		
+		userTaskDT = setUserTask(taskDetails);
+		
+		return userTaskDT;
+	}
+
+	/**
+	 * @author Daryl
+	 * Adds Task to the list and writes to file the updated list
+	 * @param input
+	 * @return Task
+	 * @throws InvalidInputException
+	 * @throws NumberFormatException
+	 * @throws IOException
+	 */
+	public static String processAdd(String input) throws NumberFormatException {
+		TaskDT userTask = null;
+		try {
+			userTask = parseTask(input);
+		} catch (InvalidInputException e) {
+			return INVALID_INPUT;
+		}
+		list.addToList(userTask);
+		fileHandler.updateFile(list);
+		return userTask.toString();
+	}
+	
+	/**
+	 * Creates and sets userTask with all the information stored in
+	 * an array
+	 * @param taskDetails
+	 * @return userTaskDT with all the task details
+	 */
+	
+	private static TaskDT setUserTask(String[] taskDetails) {
+		TaskDT userTask = new TaskDT(taskDetails[INDEX_OF_DESC]);
+		DateTime startTime = convertStringToDateTime(taskDetails[INDEX_OF_START_TIME_STRING]);
+		DateTime endTime = convertStringToDateTime(taskDetails[INDEX_OF_END_TIME_STRING]);
+		DateTime date = convertStringToDateTime(taskDetails[INDEX_OF_DATE_STRING]);
+		userTask.setStartTime(startTime);
+		userTask.setEndTime(endTime);
+		userTask.setDate(date);
+		userTask.setLocation(taskDetails[INDEX_OF_LOCATION_STRING]);
+		return userTask;
+	}
+	
+	/**
+	 * Calls helper methods based on what keyword type it is
+	 * @param keywordType
+	 * @param input
+	 * @return String array with all the relevant task details
+	 * @throws InvalidInputException
+	 */
+
+	private static String[] keywordHandler (int keywordType, String input) throws InvalidInputException {
+	
+		int spaceIndex = input.indexOf(" ");
+		if (spaceIndex == NOT_FOUND) {
+			spaceIndex = input.length();
+		}
+		switch (keywordType) {
+			case 0:
+			case 1:
+				taskDetails[INDEX_OF_START_TIME_STRING] = retrieveStartTime(input, spaceIndex);
+				break;
+			case 2:
+			case 3:
+				taskDetails[INDEX_OF_END_TIME_STRING] = retrieveEndTime(input,spaceIndex);
+				break;
+			case 4:
+				taskDetails[INDEX_OF_LOCATION_STRING] = retrieveLocation(input);
+				break;
+			case 5:
+				taskDetails[INDEX_OF_END_TIME_STRING] = retrieveEndTime(input,spaceIndex);
+				break;
+			case 6:
+				taskDetails[INDEX_OF_DATE_STRING] = retrieveDate(input);
+				break;
+		}
+
+		return taskDetails;
+	}
+	
+	/**
+	 * Retrieves the start time (first word) from input in the DateTime format (HH:MM)
+	 * @param input
+	 * @param spaceIndex
+	 * @return startTimeString
+	 */
+	
+	private static String retrieveStartTime (String input, int spaceIndex) {
+		String startTimeString = (input.substring(0, spaceIndex));
+		startTimeString = parseTimeStringFromInput(startTimeString);
+		return startTimeString;
+	}
+	
+	/**
+	 * Retrieves the end time (first word) from input in the DateTime format (HH:MM)
+	 * @param input
+	 * @param spaceIndex
+	 * @return endTimeString
+	 */
+	
+	private static String retrieveEndTime (String input, int spaceIndex) {
+		String endTimeString = (input.substring(0, spaceIndex));
+		endTimeString = parseTimeStringFromInput(endTimeString);
+		return endTimeString;
+	}
+	
+	/**
+	 * Retrieves location from input
+	 * @param input
+	 * @param spaceIndex
+	 * @return startTimeString
+	 */
+	
+	private static String retrieveLocation (String input) {
+		String location = null;
+		if (getFirstKeyword(input) == NOT_FOUND) {
+			location = input;
+		} else {
+			location = getWordsBeforeNextKeyword(input, keywords[getFirstKeyword(input)]);
+		}
+		
+		return location;
+	}
+	
+	/**
+	 * Retrieves the Date from input in the DateTime format (YYYY-MM-DD)
+	 * @param input
+	 * @param spaceIndex
+	 * @return dateString
+	 */
+	
+	private static String retrieveDate (String input) throws InvalidInputException {
+		String dateString = parseDateString(input);
+		return dateString;
+	}
+	
+
+	/**
+	 * Returns words before the specified keyword
+	 * @param input
+	 * @param keyword
+	 * @return wordsBeforeNextKeyword
+	 */
+
+	private static String getWordsBeforeNextKeyword(String input, String keyword) {
+		String wordsBeforeNextKeyword;
+		wordsBeforeNextKeyword = input.substring(0, input.indexOf(keyword));
+		return wordsBeforeNextKeyword;
+	}
+	
+	/**
+	 * Checks whether there are any keywords in the string
+	 * @param input
+	 * @return true for yes/false for no
+	 */
+
+	private static Boolean keywordIsInString(String input) {
+		for (String keyword : keywords) {
+			if (input.contains(keyword)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Retrieves the first keyword in the string
+	 * @param input
+	 * @return
+	 */
+
+	private static int getFirstKeyword(String input) {
+		int firstKeywordPos = input.length();
+		int firstKeyword = NOT_FOUND;
+
+		for (int i = 0; i < keywords.length; i++) {
+			if (input.contains(keywords[i])) {
+				if (input.indexOf(keywords[i]) < firstKeywordPos) {
+					firstKeywordPos = input.indexOf(keywords[i]);
+					firstKeyword = i;
+				}
+			}
+		}
+		return firstKeyword;
+	}
+	
+	/**
+	 * Splits the input into 2, one portion before the keyword, the other after
+	 * @param input
+	 * @param keyword
+	 * @return
+	 */
+
+	private static String[] splitByKeyword(String input, String keyword) {
+		String[] splitWords = null;
+		Pattern pattern = Pattern.compile(Pattern.quote(keyword));
+		splitWords = pattern.split(input);
+
+		return splitWords;
+	}
+}
---
+++ src/todomato/Processor.java
@@ -0,0 +1,246 @@
+package todomato;
+
+import hirondelle.date4j.DateTime;
+
+import java.io.IOException;
+import java.util.Stack;
+import java.util.TimeZone;
+
+/**
+ * This class stores information needed to process a user's command,
+ * and contains methods to parse a command's arguments. 
+ * 
+ */
+public class Processor {
+
+	protected static String fileLoc = "C:\\Users\\Joyce\\Documents\\Year 2\\test.txt";
+	//protected static String fileLoc = "D:\\test.txt";
+	protected static FileHandler fileHandler = new FileHandler(fileLoc);
+	protected static TaskDTList list = fileHandler.readFile();
+	protected static Stack<TaskDTList> oldLists = new Stack<TaskDTList>();
+	protected static final int NO_OF_CHAR_IN_HOUR_AND_MINUTE = 4;
+	protected static final int POS_OF_MINUTE = 2;
+	protected static final String INVALID_TIME_FORMAT = "Invalid Date Format";
+	protected static final int SPACE_NOT_FOUND = -1;
+
+	/**
+	 * This stores a copy of the current list before modifications are made
+	 * for possible undo operations in the future.
+	 */
+	protected static void storeCurrentList() {
+		TaskDTList lastList = new TaskDTList();
+		lastList.deepCopy(list);
+		oldLists.push(lastList);
+	}
+
+	public static TaskDTList getList() {
+		return list;
+	}
+
+	
+	/**
+	 * Converts "2" "1" to "YYYY-MM-DD"
+	 * @author Daryl
+	 * @param String month, String day
+	 * 
+	 * @return userDate
+	 * @throws IOException
+	 */
+	protected static String convertDateToStandardForm(String month, String day) {
+		String year = Integer.toString(DateTime.now(TimeZone.getTimeZone("GMT+8:00")).getYear());
+		if (month.length() == 1) {
+			month = "0" + month;
+		}
+		if (day.length() == 1) {
+			day = "0" + day;
+		}
+		return year + "-" + day + "-" + month;
+	}
+
+	/**
+	 * Returns time from user string i.e. 19 hours 30 minutes from "1930" or
+	 * "730pm" or "0730pm"
+	 * 
+	 * @param input
+	 * @return userTime
+	 * @throws InvalidInputException
+	 */
+	protected static Time parseTimeFromString(String input)
+			throws InvalidInputException {
+		Time userTime = null;
+		try {
+			if (input == null) {
+				return null;
+			}
+			int hour = 0, minute = -1;
+			String meridiem[] = new String[] { "am", "pm" };
+			int meridiemIndex = checkMeridiem(input);
+			if (meridiemIndex != -1) {
+				input = input.substring(0,
+						input.indexOf(meridiem[meridiemIndex]));
+				if (input.length() == 1) {
+					hour = Integer.parseInt(input);
+					minute = 0;
+				} else if (input.length() == 3) {
+					hour = Integer.parseInt(input.substring(0, 1));
+					minute = Integer.parseInt(input
+							.substring(POS_OF_MINUTE - 1));
+				} else if (input.length() == 4) {
+					hour = Integer.parseInt(input.substring(0, 2));
+					minute = Integer.parseInt(input.substring(POS_OF_MINUTE));
+				} else {
+					throw new InvalidInputException(INVALID_TIME_FORMAT);
+				}
+				if (meridiemIndex == 1) {
+					hour += 12;
+				}
+				userTime = new Time(hour, minute);
+			} else {
+				String userHour = null;
+				String userMinute = null;
+				if ((input.length() == 1) || (input.length() == 2)) {
+					userHour = input;
+					userTime = new Time(Integer.parseInt(input));
+				} else if (input.length() == NO_OF_CHAR_IN_HOUR_AND_MINUTE) {
+					userHour = input.substring(0, POS_OF_MINUTE);
+					userMinute = input.substring(POS_OF_MINUTE);
+					userTime = new Time(Integer.parseInt(userHour),
+							Integer.parseInt(userMinute));
+				} else if (input.length() == 3) {
+					userHour = input.substring(0, 1);
+					userMinute = input.substring(1);
+					userTime = new Time(Integer.parseInt(userHour));
+				} else {
+					throw new InvalidInputException(INVALID_TIME_FORMAT);
+				}
+			}
+		} catch (NumberFormatException e) {
+			return null;
+		}
+		return userTime;
+	}
+	
+	/**
+	 * Checks whether "am" or "pm" is in the string
+	 * @param input
+	 * @return integer indicating which meridiem is present
+	 * -1 if there is not one
+	 */
+
+	protected static int checkMeridiem(String input) {
+		String meridiems[] = new String[] { "am", "pm" };
+		for (int i = 0; i < meridiems.length; i++) {
+			if (input.contains(meridiems[i])) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	/**
+	 * Converts "Jan 1" to "YYYY-MM-DD" (DateTime format)
+	 * @param input
+	 * @return standardFormDate
+	 * @throws InvalidInputException
+	 */
+	protected static String parseDateString(String input) {
+		if (DateTime.isParseable(input)) {
+			return input;
+		}
+		if (input == null) {
+			return null;
+		}
+		input = input.toLowerCase();
+		String[] parts = input.split(" ");
+		String standardFormDate = null;
+		String[] months = new String[] { "jan", "feb", "mar", "apr", "may",
+				"jun", "jul", "aug", "sep", "oct", "nov", "dec" };
+		for (int i = 0; i < months.length; i++) {
+			if (parts.length > 1) {
+				if (parts[0].contains(months[i])) {
+					standardFormDate = convertDateToStandardForm(
+							parts[1], String.valueOf(i + 1));
+				} else if (parts[1].contains(months[i])) {
+					standardFormDate = convertDateToStandardForm(
+							parts[0], String.valueOf(i + 1));
+				}
+			}
+		}
+		
+		return standardFormDate;
+	}
+	
+	/**
+	 * Converts "930pm" to HH:MM (DateTime Format)
+	 * @param input
+	 * @return standardFormDate
+	 * @throws InvalidInputException
+	 */
+	
+	protected static String parseTimeStringFromInput(String input) {
+		String TimeString = null;
+		if (input == null) {
+			return "";
+		}
+		String meridiem[] = new String[] { "am", "pm" };
+		String userHour = null;
+		String userMinute = "00";
+		int meridiemIndex = checkMeridiem(input);
+		if (meridiemIndex != -1) {
+			input = input.substring(0,
+					input.indexOf(meridiem[meridiemIndex]));
+			if (input.length() == 1) {
+				userHour = input;
+			} else if (input.length() == 3) {
+				userHour = input.substring(0, 1);
+				userMinute = input.substring(POS_OF_MINUTE - 1);
+			} else if (input.length() == 4) {
+				userHour = input.substring(0, 2);
+				userMinute = input.substring(POS_OF_MINUTE - 2);
+			}
+			if (meridiemIndex == 1) {
+				userHour = Integer.toString(Integer.parseInt(userHour) + 12);
+			}
+		} else {
+			if (input.length() == 5 && input.contains(":")) {
+				return input;
+			} else {
+				if ((input.length() == 1) || (input.length() == 2)) {
+					userHour = input;
+				} else if (input.length() == NO_OF_CHAR_IN_HOUR_AND_MINUTE) {
+					userHour = input.substring(0, POS_OF_MINUTE);
+					userMinute = input.substring(POS_OF_MINUTE);
+				} else if (input.length() == 3) {
+					userHour = input.substring(0, 1);
+					userMinute = input.substring(1);
+				}
+			}
+		}
+		if (userHour.length() == 1) {
+			userHour = "0" + userHour;
+		}
+		TimeString = userHour + ":" + userMinute;
+		return TimeString;
+	}
+	
+	/**
+	 * Converts strings of form YYYY-MM-DD or
+	 * HH:MM to DateTime format
+	 * @param input
+	 * @return DateTime
+	 */
+	
+	protected static DateTime convertStringToDateTime(String input) {
+		DateTime userDateTime = null;
+		if (input == null) {
+			return null;
+		} else {
+			if (!DateTime.isParseable(input)) {
+				return null;
+			} else {
+				userDateTime = new DateTime(input);
+			}
+		}
+		return userDateTime;
+	}
+	
+}
