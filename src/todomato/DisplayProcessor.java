a6bc5860340de53babf9539dca81dec850628dd7
Foo Lin Xuan <linxuan1994@gmail.com>
2014-03-23 23:19:55 +0800

    fixed bug in display by date

--- src/todomato/DisplayProcessor.java
+++ src/todomato/DisplayProcessor.java
@@ -1,22 +1,27 @@
 package todomato;
 
 /**
- * This class contains methods to process display commands by the user. It is
- * used to display all of a user's tasks when using the command line, but the
- * GUI automatically displays the user's tasks after each command. It also
- * creates notifications about upcoming tasks.
+ * This class contains methods to process display commands by the user. 
+ * It updates the user's list of tasks, and saves it to disk.
+ * 
+ * <p>
+ * The following ways to display are supported: 
+ * <ul>
+ * <li> display all tasks
+ * <ul> <li> "display" </ul>
+ * <li> display tasks by date
+ * <ul> <li> "display by date" </ul>
+ * </ul>
  * 
  */
 public class DisplayProcessor extends Processor {
 	private static final String ARGUMENT_SORT_BY_DATE = "by date";
-	// private static final String ARGUMENT_NOTIFICATION = "notification";
 	private static final String SUCCESS_SORT_BY_DATE = "Sorted by date";
 	private static final String INVALID_ARGUMENT_MESSAGE = "Invalid argument";
-
-	// private static TaskDTList notify = new TaskDTList();
+	
 	/**
 	 * @author linxuan
-	 * @return TaskList
+	 * @return Status message telling user if command was successfully executed 
 	 */
 	public static String processDisplay(String argument) {
 		displayList = list;
@@ -27,10 +32,6 @@ public class DisplayProcessor extends Processor {
 		if (argument.equalsIgnoreCase(ARGUMENT_SORT_BY_DATE)) {
 			return sortByDate();
 		}
-		// if (argument.equalsIgnoreCase(ARGUMENT_NOTIFICATION)) {
-		// return notify().toString();
-		// }
-		// TODO
 		return INVALID_ARGUMENT_MESSAGE;
 	}
 
@@ -44,23 +45,15 @@ public class DisplayProcessor extends Processor {
 	 * @return status message
 	 */
 	private static String sortByDate() {
-		// TODO
-		// may need for displaying: stores original indices before sorting
-		int[] orderOfIndices = new int[list.getSize()];
-		initArray(orderOfIndices);
-		bubbleSort(orderOfIndices);
+		bubbleSort();
 		fileHandler.updateFile(list);
 		return SUCCESS_SORT_BY_DATE;
 	}
 
-	private static void bubbleSort(int[] orderOfIndices) {
+	private static void bubbleSort() {
 		for (int i = 0; i < (list.getSize() - 1); i++) {
 			for (int j = 0; j < (list.getSize() - i - 1); j++) {
-				// if (array[d] > array[d+1]) /* For descending order use < */
-				if (compare(i, j)) {
-					int swap = orderOfIndices[j];
-					orderOfIndices[j] = orderOfIndices[j + 1];
-					orderOfIndices[j + 1] = swap;
+				if (compare(j, j + 1)) {
 					list.swap(j, j + 1);
 				}
 			}
@@ -68,13 +61,14 @@ public class DisplayProcessor extends Processor {
 	}
 
 	private static boolean compare(int i, int j) {
-		return list.getListItem(j).getDate()
-				.compareTo(list.getListItem(j + 1).getDate()) > 0;
-	}
-
-	private static void initArray(int[] orderOfIndices) {
-		for (int i = 0; i < orderOfIndices.length; i++) {
-			orderOfIndices[i] = i + 1;
-		}
+		if(list.getListItem(i).getDate() == null) {
+			return false;
+		} if(list.getListItem(j).getDate() == null) {
+			return true;
+		} if(list.getListItem(i).getDate()
+				.compareTo(list.getListItem(j).getDate()) > 0) {
+			return true;
+		} 
+		return false;
 	}
 }
