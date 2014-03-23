ab153a1ff055bbc9955d0910d05490a993704da0
Joyce Woo <yyjoycewoo@gmail.com>
2014-03-23 20:16:11 +0800

    Sorry, forget to add files on previous commits

---
+++ src/todomato/RedoProcessor.java
@@ -0,0 +1,31 @@
+package todomato;
+
+
+/** 
+ * This class contains methods to process undo commands by the user.
+ * It updates the user's lists of tasks, and saves it to disk.
+ * @author Joyce
+ *
+ */
+public class RedoProcessor extends Processor {
+	 private static final String NO_CHANGES_TO_REDO_MSG = "No changes to redo";
+	 private static final String SUCCESS_MSG = "Last action redone";
+
+		/**
+		 * Undo last action, if possible
+		 * @author Joyce
+		 * @return Status message telling user if there were changes to redo
+		 */
+		public static String processRedo() {
+			displayList = list;
+			
+			if (!redoList.isEmpty()) {
+				list = redoList.pop();	
+				fileHandler.updateFile(list);
+				return SUCCESS_MSG;
+			} else {
+				return NO_CHANGES_TO_REDO_MSG;
+			}
+		}
+
+ }
