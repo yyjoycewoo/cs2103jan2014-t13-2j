6ce7303cc921ceba475e87545249dcb5c036828c
Joyce Woo <yyjoycewoo@gmail.com>
2014-04-03 09:15:12 +0800

    Added tests for find, couldn't get it to work in a new method, so I put it all in the same one

--- src/tests/IntegrationTest.java
+++ src/tests/IntegrationTest.java
@@ -7,21 +7,22 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.rules.TemporaryFolder;
 
 import todomato.FileHandler;
+import todomato.FindProcessor;
+import todomato.Processor;
 import todomato.SplitProcessorsHandler;
 import todomato.InvalidInputException;
+import todomato.Task;
 import todomato.TaskList;
 
 public class IntegrationTest {
-	private  File tasks;
+	private File tasks;
 	
 	@Rule 
-	public  TemporaryFolder folder= new TemporaryFolder();
+	public TemporaryFolder folder= new TemporaryFolder();
 	
 	@Before
 	public void createTestData() throws IOException {
@@ -36,23 +37,53 @@ public class IntegrationTest {
 
 	
 	@Test
-	public void testAddDelete() throws InvalidInputException, IOException {
+	public void testIntegrated() throws InvalidInputException, IOException {
 		String fileLoc = "tasks.txt";
+		String command = "";
 		FileHandler fileHandler = new FileHandler(fileLoc);
 		TaskList list = fileHandler.readFile();
+		
 		assertEquals("cs post-lecture quiz", list.getListItem(0).getDescription());
 		String messageAdd1 = SplitProcessorsHandler.processCommand("add something");
 		assertEquals("Added something", messageAdd1);
+		
 		String messageUndo1 = SplitProcessorsHandler.processCommand("undo");
 		assertEquals("Last action undone", messageUndo1);
-		SplitProcessorsHandler.processCommand("add something1");
-		String messageAdd2 = SplitProcessorsHandler.processCommand("add Tutorial @ERC-SR2 on 5 Feb at 2pm");
+		
+		String messageUndo2 = SplitProcessorsHandler.processCommand("undo");
+		assertEquals("No changes to undo", messageUndo2);
+		
+		String messageRedo1 = SplitProcessorsHandler.processCommand("redo");
+		assertEquals("Last action redone", messageRedo1);
+		
+		String messageRedo2 = SplitProcessorsHandler.processCommand("redo");
+		assertEquals("No changes to redo", messageRedo2);
+
+		//test case of a task that does not exist
+		String messageFind1 = SplitProcessorsHandler.processCommand("find AONETUHEONUTHOu");
+		assertEquals("No tasks found", messageFind1);
+
+		//test boundary case of searching an empty string
+		String messageFind2 = FindProcessor.processFind("");
+		assertEquals("Search completed", messageFind2);
+		String original_list = Processor.getList().toString();
+		String found_list = Processor.getDisplayList().toString();
+		assertEquals(original_list, found_list);	
+
+		String messageAdd2 = SplitProcessorsHandler.processCommand("add Tutorial @ERC-SR2 on 5 Feb at 2pm");		
 		assertEquals("Added Tutorial at 14:00 on Feb 05 2014 in ERC-SR2", messageAdd2);
+
+		//test case of a task that does exist 
+		command = "tutorial";
+		String messageFind3 = FindProcessor.processFind(command);
+		assertEquals("Search completed", messageFind3);
+		
 		String messageUpdate1 = SplitProcessorsHandler.processCommand("update 4 desc Dinner with Parents\\ location home\\");
 		assertEquals("Dinner with Parents in home", messageUpdate1);
+		
 		String messageDelete1 = SplitProcessorsHandler.processCommand("delete all");
 		assertEquals("Deleted: 5 task(s)", messageDelete1);
+		assertEquals(Processor.getList().toString(), "");
 	}
-	
 	
 }
