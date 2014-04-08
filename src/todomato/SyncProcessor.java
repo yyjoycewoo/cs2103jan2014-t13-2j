8a19c7c0eb062ec25c987d24a463ce111b9285d8
DarylHo <DarylHo@localhost>
2014-04-08 21:27:36 +0800

    now dialogue box accurately pops up when username and password is null even when there's previous text files

--- src/todomato/SyncProcessor.java
+++ src/todomato/SyncProcessor.java
@@ -3,8 +3,10 @@ package todomato;
 public class SyncProcessor extends Processor {
 
 	public static String processSync() {
-		if (list.getUserName() == null && list.getPassword() == null) {
+		if (list.getUserName() == null && list.getPassword() == null ||
+		(list.getUserName().equals("null") && list.getPassword().equals("null"))){
 			new UsernamePasswordDialogueBox();
+			fileHandler.updateFile(list);
 		}
 		
 		return "syncing...";
