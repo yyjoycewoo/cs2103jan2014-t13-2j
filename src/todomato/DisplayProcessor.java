99fda15fb3a7703a24d2a5c365c1f6a986fe7561
Hao Eng <this9is3me@gmail.com>
2014-04-10 16:31:46 +0800

    Added author

---
+++ src/todomato/DisplayProcessor.java
@@ -0,0 +1,46 @@
+//@author A0101578H
+package todomato;
+
+import hirondelle.date4j.DateTime;
+
+/**
+ * This class contains methods to process display commands by the user. 
+ * 
+ * <p>
+ * The following ways to display are supported: 
+ * <ul>
+ * <li> display all tasks
+ * <ul> <li> "display" </ul>
+ * </ul>
+ * 
+ */
+public class DisplayProcessor extends Processor {
+	private static final String SUCCESS_DISPLAY = "All tasks have been displayed: ";
+	//private static final String INVALID_ARGUMENT_MESSAGE = "Invalid argument";
+	
+	/**
+	 * @param argument 
+	 * @return Status message along with a String of the list 
+	 */
+	public static String processDisplay() {
+		displayList.deepCopy(list);
+		return SUCCESS_DISPLAY + displayList.toString();
+	}
+
+	public static void displayBetweenDates(DateTime startDate, DateTime endDate) {
+		if (startDate == null && endDate == null) {
+			displayList.deepCopy(list);
+		} else {
+			TaskList currTasks = new TaskList();
+			for (Task i : list.getList()) {
+				DateTime date = i.getEndDate();
+				if (date == null)
+					currTasks.addToList(i);
+				else if (date.numDaysFrom(startDate) <= 0 && date.numDaysFrom(endDate) >= 0) {
+					currTasks.addToList(i);
+				}
+			}
+			displayList.deepCopy(currTasks);
+		}
+	}
+}
