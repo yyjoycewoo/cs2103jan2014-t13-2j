b6aa1666784fab7160b480e7ada8a3d6e2c3e26e
tiotao <tiotao@localhost>
2014-04-11 16:27:55 +0800

    switch server to heroku server

---
+++ src/todomato/Command.java
@@ -0,0 +1,63 @@
+package todomato;
+/**
+ * This class stores the different components of a user's command: the action, and the argument.
+ * The action is the keyword specified in the first word of a command (eg. add, delete, update,
+ * etc), and the argument is the rest of the command.
+ * @author Joyce
+ *
+ */
+public class Command {
+	private static final String EMPTY_STRING = "";
+	private String action;
+	private String argument;
+	
+	/**
+	 * Create a new Command object with the specified action and argument as an empty String
+	 * @param action The action that the user wants to perform
+	 */
+	public Command(String action) {
+		this(action, "");
+	}
+	
+	/**
+	 * Create a new Command object with specified action and argument
+	 * @param action The action that the user wants to perform
+	 * @param argument The argument that the user wants to do with the action
+	 */
+	public Command(String action, String argument) {
+		this.setAction(action);
+		this.setArgument(argument);
+	}
+
+	/**
+	 * Create a new Command object with specified input
+	 * @param input Array with action and argument that the user wants to execute
+	 */
+	public Command(String[] input) {
+		//command is the first word that the user enters
+		this.setAction(input[0]);
+		
+		//argument is everything except the first word that the user enters
+		if (input.length > 1) {
+			this.setArgument(input[1]);
+		} else {
+			this.setArgument(EMPTY_STRING);
+		}
+	}
+
+	public String getAction() {
+		return action;
+	}
+
+	public void setAction(String action) {
+		this.action = action;
+	}
+
+	public String getArgument() {
+		return argument;
+	}
+
+	public void setArgument(String argument) {
+		this.argument = argument;
+	}
+}
