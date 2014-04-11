d9f42ab96c87d2bace75355dd7824d0d4e2b84d8
Hao Eng <this9is3me@gmail.com>
2014-04-11 18:49:52 +0800

    Allows the notification to pop up for certain time of current date

---
+++ src/todomato/KeepChecking.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package todomato;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+
+//@author A0101324A
+
+/*
+ * Checking the notify time for each task (if possible) and enable pop up 
+ * if the current time matches the notify time for that task
+ */
+public class KeepChecking implements Job {
+	TaskList today_pop = new TaskList();
+
+	@Override
+	public void execute(JobExecutionContext context) {
+		today_pop.clearList();
+		// getting current time
+		String timeStamp = new SimpleDateFormat("HH:mm").format(Calendar
+				.getInstance().getTime());
+
+		for (int i = 0; i < Processor.getList().getSize(); i++) {
+			// if there is notify time for that task
+			if (Processor.getList().getListItem(i).getNoticeTime() != null) {
+				// if the current time matches the notify time
+				if (Processor.getList().getListItem(i).getNoticeTime()
+						.toString().contains(timeStamp)) {
+					// pop up the relevant task at that time
+					today_pop.addToList(Processor.getList().getListItem(i));
+				}
+			}
+		}
+		if (today_pop.getSize() != 0) {
+			// inform the notification to pop up the relevant tasks for today
+			Notification.popUpNotice(today_pop);
+		}
+	}
+}
