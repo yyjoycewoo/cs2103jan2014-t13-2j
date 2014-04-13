5caa7f61071c45b639fbe4c49c538d357bf23def
Hao Eng <this9is3me@gmail.com>
2014-04-13 15:40:42 +0800

    Remove magic numbers

---
+++ src/tests/DeleteProcessorTest.java
@@ -0,0 +1,127 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Field;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import todomato.DeleteProcessor;
+import todomato.FileHandler;
+import todomato.InvalidInputException;
+import todomato.Processor;
+import todomato.TaskList;
+
+public class DeleteProcessorTest {
+	private  File tasks;
+	
+	@Rule 
+	public  TemporaryFolder folder= new TemporaryFolder();
+	
+	@Before
+	public void createTestData() throws IOException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		System.setProperty("user.dir", folder.getRoot().toString());
+		tasks = folder.newFile("tasks.txt");
+		BufferedWriter out = new BufferedWriter(new FileWriter(tasks));
+		out.write("null\r\n");
+        out.write("null\r\n");
+        out.write("null\r\n");
+        out.write("Lunch#null#null#null#null#null#0#1046042885#2014-04-09 16:19:17.842000000#HIGH#true#null#null#null\r\n");
+        out.write("Dinner#null#18:00#2014-04-09#2014-04-10#null#0#-1351579072#2014-04-09 16:18:25.784000000#LOW#false#null#null#null\r\n");
+        out.write("Project meeting#null#14:00#2014-04-01#2014-04-10#null#0#570051783#2014-04-09 16:18:48.669000000#MEDIUM#true#null#null#null\r\n");
+        out.close();
+        
+        FileHandler fileHandler = new FileHandler("tasks.txt");
+        TaskList newList = fileHandler.readFile();
+        //System.out.println(newList.toString());
+        Field f = Processor.class.getDeclaredField("list");
+        f.setAccessible(true);
+        if ("list".equals(f.getName())) {
+            f.setAccessible(true);
+            //System.out.println(f.get("list"));
+            f.set("list", newList);
+            //System.out.println(f.get("list"));
+        }
+	}
+
+	@Test
+	public void testDeleteAll() throws InvalidInputException, IOException {
+		String message = DeleteProcessor.processDelete("all");
+		assertEquals("Deleted: 3 task(s)", message);
+	}
+	
+	@Test
+	public void testDeleteSingle() throws InvalidInputException, IOException {
+		String message = DeleteProcessor.processDelete("1");
+		assertEquals("Deleted: Lunch", message);
+	}
+	
+	// This is a boundary case for the valid indices partition
+	@Test
+	public void testDeleteMultiple() throws InvalidInputException, IOException {
+		String message = DeleteProcessor.processDelete("3,1");
+		assertEquals("Deleted: 2 task(s)", message);
+	}
+	
+	@Test
+	public void testDeleteStartDate() throws InvalidInputException, IOException {
+		String message = DeleteProcessor.processDelete("startdate 1 apr");
+		assertEquals("Deleted: 1 task(s)", message);
+	}
+	
+	@Test
+	public void testDeleteEndDate() throws InvalidInputException, IOException {
+		String message = DeleteProcessor.processDelete("enddate 10 apr");
+		assertEquals("Deleted: 2 task(s)", message);
+	}
+	
+	@Test
+	public void testDeleteRange() throws InvalidInputException, IOException {
+		String message = DeleteProcessor.processDelete("1-3");
+		assertEquals("Deleted: 3 task(s)", message);
+	}
+	
+	// This is a boundary case for the invalid indices partition
+	@Test
+	public void testExceedMaxIndex() throws InvalidInputException {
+		String message = DeleteProcessor.processDelete("6");
+		assertEquals("Delete failed: Index out of bound", message);
+	}
+	
+	// This is a boundary case for the invalid indices partition
+	@Test
+	public void testNegativeIndex() throws InvalidInputException {
+		String message = DeleteProcessor.processDelete("-1");
+		assertEquals("Delete failed: Index not in number format", message);
+	}
+		
+	// This is a test for the empty list partition
+	@Test
+	public void testEmptyList() throws InvalidInputException {
+		try {
+			String message = DeleteProcessor.processDelete("all");
+			assertEquals("Deleted: 3 task(s)", message);
+			DeleteProcessor.processDelete("1");
+			fail("Should have thrown InvalidInputException");
+		} catch (InvalidInputException e){
+			assertEquals(e.getMessage(), "empty list");
+		}
+	}
+	
+	@Test
+	public void testInvalidNumberOfLimits() throws InvalidInputException {
+		try {
+			DeleteProcessor.processDelete("-");
+			fail("Should have thrown InvalidInputException");
+		} catch (InvalidInputException e){
+			assertEquals(e.getMessage(), "Upper and lower limits required");
+		}
+	}
+}
---
+++ src/todomato/DeleteProcessor.java
@@ -0,0 +1,238 @@
+//@author A0101578H
+package todomato;
+
+import hirondelle.date4j.DateTime;
+
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * This class contains methods to process delete commands by the user.
+ * It updates the user's lists of tasks, and saves it to disk.
+ * 
+ * <p>
+ * The following ways to delete are supported:
+ * <ul>
+ * <li> delete a single task by specifying the index
+ * <ul> <li> "delete 1" </ul>
+ * <li> delete multiple tasks with indices in order
+ * <ul> <li> "delete 1,2,3" </ul>
+ * <li> delete all tasks
+ * <ul> <li> "delete all" </ul>
+ * <li> delete tasks that starts on a date
+ * <ul> <li> "delete startdate 1 jan" </ul>
+ * <li> delete tasks that ends on a date
+ * <ul> <li> "delete enddate 20 jan" </ul>
+ * <li> delete tasks that are completed
+ * <ul> <li> "delete completed" </ul>
+ * </ul>
+ *
+ *
+ */
+
+public class DeleteProcessor extends Processor {
+	private static final String DELIMITER_FOR_ARGUMENT = "\\s*(,| )\\s*";
+	private static final String DELIMITER_FOR_RANGE = "\\s*-\\s*";
+	private static final String IDENTIFIER_RANGE = "-";
+	private static final String ARGUMENT_START_DATE = "startdate";
+	private static final String ARGUMENT_END_DATE = "enddate";
+	private static final String ARGUMENT_ALL = "all";
+	private static final String ARGUMENT_COMPLETE = "complete";
+	private static final String TASKS = " task(s)";
+	private static final String SUCCESSFUL_DELETE = "Deleted: ";
+	private static final String INVALID_INPUT_EMPTY_LIST = "empty list";
+	private static final String INVALID_INPUT_MISSING_ARGUMENT = "Missing argument";
+	private static final String INVALID_NUMBER_OF_LIMITS = "Upper and lower limits required";
+	private static final String ERROR_MESSAGE_NUMBER_FORMAT = "Delete failed: Index not in number format";
+	private static final String ERROR_MESSAGE_INDEX_OUT_OF_BOUND = "Delete failed: Index out of bound";
+	
+	//private static final Logger logger = Logger.getLogger(DeleteProcessor.class.getName());
+	
+	
+	/**
+	 * @param argument
+	 * @return String of success/error message accordingly 
+	 */
+	public static String processDelete(String argument) throws InvalidInputException {
+		//logger.log(Level.INFO, "processing delete");
+		if (list.getSize() == 0) {
+			//logger.log(Level.INFO, "exited due to empty list");
+			throw new InvalidInputException(INVALID_INPUT_EMPTY_LIST);
+		}
+		if (argument.isEmpty()) {
+			//logger.log(Level.WARNING, "exited due to missing argument");
+			throw new InvalidInputException(INVALID_INPUT_MISSING_ARGUMENT);
+		}
+		
+		storeCurrentList();
+		
+		String[] argArr = argument.split(DELIMITER_FOR_ARGUMENT);
+		String argStr = argArr[0];
+		argStr.toLowerCase();
+		String statusMessage;
+		try {
+			if (argStr.equalsIgnoreCase(ARGUMENT_ALL)) {
+				statusMessage = SUCCESSFUL_DELETE + deleteAll() + TASKS; 
+			}
+			else if (argStr.contains(ARGUMENT_COMPLETE)) {
+				statusMessage = SUCCESSFUL_DELETE + deleteCompleted() + TASKS;
+			}
+			else if (argStr.equalsIgnoreCase(ARGUMENT_START_DATE)) {
+				statusMessage = SUCCESSFUL_DELETE + deleteStartDate(argArr) + TASKS;
+			}
+			else if (argStr.equalsIgnoreCase(ARGUMENT_END_DATE)) {
+				statusMessage = SUCCESSFUL_DELETE + deleteEndDate(argArr) + TASKS;
+			}
+			else if (argument.contains(IDENTIFIER_RANGE)) {
+				statusMessage = SUCCESSFUL_DELETE + deleteRange(argument) + TASKS;
+			}
+			else if (argArr.length > 1) {
+				statusMessage = SUCCESSFUL_DELETE + deleteMultiple(argArr) + TASKS;
+			}
+			else {
+				statusMessage = SUCCESSFUL_DELETE + deleteSingle(argStr);
+			}
+			
+			displayList = list;
+			
+			fileHandler.updateFile(list);
+			
+			//logger.log(Level.INFO, "end of processing");
+			return statusMessage;
+		} catch(NumberFormatException e) {
+			UndoProcessor.processUndo();
+			//logger.log(Level.WARNING, "exited due to non-number");
+			return ERROR_MESSAGE_NUMBER_FORMAT;
+		} catch(IndexOutOfBoundsException e) {
+			UndoProcessor.processUndo();
+			//logger.log(Level.WARNING, "exited due to index out of bound");
+			return ERROR_MESSAGE_INDEX_OUT_OF_BOUND;
+		}		
+	}
+
+	private static String deleteRange(String argument) throws InvalidInputException {
+		//logger.log(Level.INFO, "processing delete range");
+		String[] rangeLimits = argument.split(DELIMITER_FOR_RANGE);
+		if (!(rangeLimits.length == 2)) {
+			throw new InvalidInputException(INVALID_NUMBER_OF_LIMITS);
+		}
+		//logger.log(Level.INFO, "number of limits valid");
+		int lowerLimitIndex = Integer.parseInt(rangeLimits[0]) - 1;
+		int upperLimitIndex = Integer.parseInt(rangeLimits[1]) - 1;
+		int numberOfTasksDeleted = 0;
+		for (int i = upperLimitIndex; i >= lowerLimitIndex; i--) {
+			list.deleteListItem(i);
+			numberOfTasksDeleted++;
+		}
+		return Integer.toString(numberOfTasksDeleted);
+	}
+
+	private static String deleteCompleted() {
+		int numberOfTasksDeleted = 0;
+		for (int i = list.getSize() - 1; i >= 0 ; i--) { 
+			if (list.getListItem(i).getCompleted()) {
+				list.deleteListItem(i);
+				numberOfTasksDeleted++;
+			}		
+		}
+		return Integer.toString(numberOfTasksDeleted);
+	}
+
+	private static String deleteStartDate(String[] arg) {
+		int numberOfTasksDeleted = 0;
+		String date = arg[1] + " " + arg[2];
+		try {
+			date = parseDateString(date);
+		} catch (InvalidInputException e) {
+			return INVALID_DATE;
+		}
+		DateTime dateDT = convertStringToDateTime(date);
+		for (int i = list.getSize() - 1; i >= 0; i--) {
+			if(isSameStartDate(i,dateDT)) {
+				list.deleteListItem(i);
+				
+				numberOfTasksDeleted++;
+			}
+		}
+		return Integer.toString(numberOfTasksDeleted);
+	}
+	
+	private static boolean isSameStartDate(int i, DateTime dateDT) {
+		if (list.getListItem(i).getStartDate() == null) {
+			return false;
+		}
+		if (list.getListItem(i).getStartDate().isSameDayAs(dateDT)) {
+			return true;
+		}
+		return false;
+	}
+	
+	private static String deleteEndDate(String[] arg) {
+		int numberOfTasksDeleted = 0;
+		String date = arg[1] + " " + arg[2];
+		try {
+			date = parseDateString(date);
+		} catch (InvalidInputException e) {
+			return INVALID_DATE;
+		}
+		DateTime dateDT = convertStringToDateTime(date);
+		for (int i = list.getSize() - 1; i >= 0; i--) {
+			if(isSameEndDate(i,dateDT)) {
+				list.deleteListItem(i);
+				numberOfTasksDeleted++;
+			}
+		}
+		return Integer.toString(numberOfTasksDeleted);
+	}
+
+	private static boolean isSameEndDate(int i, DateTime dateDT) {
+		if (list.getListItem(i).getEndDate() == null) {
+			return false;
+		}
+		if (list.getListItem(i).getEndDate().isSameDayAs(dateDT)) {
+			return true;
+		}
+		return false;
+	}
+
+	private static String deleteAll() {
+		int numberOfTasksDeleted = 0;
+		while(list.getSize() != 0) {
+			list.deleteListItem(0);
+			
+			numberOfTasksDeleted++;
+		}
+		return Integer.toString(numberOfTasksDeleted);
+	}
+	
+	private static String deleteSingle(String indexString) {
+		int index = Integer.parseInt(indexString) - 1;
+		Task deletedTask = list.getListItem(index);
+		list.deleteListItem(index);
+		
+		return deletedTask.toString();
+	}
+	
+	private static String deleteMultiple(String[] strIndices) {
+		int[] intIndices = new int[strIndices.length];
+		for (int i = 0; i < strIndices.length; i++) {
+			intIndices[i] = Integer.parseInt(strIndices[i]);
+		}
+		Arrays.sort(intIndices);
+		reverse(intIndices);
+		for (int i = 0; i < intIndices.length; i++) {
+			list.deleteListItem(intIndices[i] - 1);
+		}
+		return Integer.toString(strIndices.length);
+	}
+	
+	private static void reverse(int[] intIndices) {
+		int length = intIndices.length;
+		for (int i = 0; i < length/2; i++) {
+			int t = intIndices[i];
+			intIndices[i] = intIndices[length - 1 - i];
+			intIndices[length - 1 - i] = t;
+		}
+	}
+}
